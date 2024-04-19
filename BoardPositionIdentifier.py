e3db41d526f8e6b3cdf64bd004edc9455d203774
JJBigDub <138677379+JJBigDub@users.noreply.github.com>
2024-04-19 21:35:57 +0530

    Add files via upload

---
+++ BoardPositionIdentifier.py
@@ -0,0 +1,126 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import pandas as pd
+import glob
+import os
+from pathlib import Path
+
+data = pd.read_csv(r'C:\Users\Kumar\Desktop\Desktop\Python\icg-freshers-data-science-competition\Dataset\Sample_submission.csv')
+
+
+def cropBoard(image):
+  crop = image[62:425, 148:511]
+  return crop
+
+def transformImage(crop):
+  kernel =  np.ones((2,2),np.uint8)
+  img_g =  cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
+  ret,thresh1 = cv2.threshold(img_g,127,255,cv2.THRESH_BINARY)
+  thresh1 = cv2.morphologyEx(thresh1, cv2.MORPH_OPEN, kernel)
+  return thresh1
+
+def isEmpty(cell_image):
+  mean, std = cv2.meanStdDev(cell_image)
+  if (std < 10):
+    return True
+  else:
+    return False
+  
+def solution(imgtest, y, data):
+  threshtest = cropBoard(imgtest)
+  #threshtest = transformImage(crop)
+  pos1 = threshtest[0:115,0:115]
+  pos2 = threshtest[0:115,121:236]
+  pos3 = threshtest[0:115,245:360]
+  pos4 = threshtest[122:237,0:115]
+  pos5 = threshtest[122:237,121:236]
+  pos6 = threshtest[122:237,245:360]
+  pos7 = threshtest[245:360,0:115]
+  pos8 = threshtest[245:360,121:236]
+  pos9 = threshtest[245:360,245:360]
+  postest1 = pos1
+  postest2 = pos2
+  postest3 = pos3
+  postest4 = pos4
+  postest5 = pos5
+  postest6 = pos6
+  postest7 = pos7
+  postest8 = pos8
+  postest9 = pos9
+  symbol1 = detect_symbol(postest1)
+  symbol2 = detect_symbol(postest2)
+  symbol3 = detect_symbol(postest3)
+  symbol4 = detect_symbol(postest4)
+  symbol5 = detect_symbol(postest5)
+  symbol6 = detect_symbol(postest6)
+  symbol7 = detect_symbol(postest7)
+  symbol8 = detect_symbol(postest8)
+  symbol9 = detect_symbol(postest9)
+  data.loc[y, 'POS_1'] = symbol1
+  data.loc[y, 'POS_2'] = symbol2
+  data.loc[y, 'POS_3'] = symbol3
+  data.loc[y, 'POS_4'] = symbol4
+  data.loc[y, 'POS_5'] = symbol5
+  data.loc[y, 'POS_6'] = symbol6
+  data.loc[y, 'POS_7'] = symbol7
+  data.loc[y, 'POS_8'] = symbol8
+  data.loc[y, 'POS_9'] = symbol9
+  print(y)
+
+def detect_symbol(cell_image):
+  image_dir_o = r'C:\Users\Kumar\Desktop\Desktop\Python\icg-freshers-data-science-competition\Zero_Template'
+  image_files_o = glob.glob(image_dir_o + '/*.png')
+  corr_o = 1000000000000000
+
+  img = transformImage(cell_image)
+  img_float = img.astype(np.float32)
+
+  for x in image_files_o:
+    image = cv2.imread(x)
+    temp_o = transformImage(image)
+    templ_o_float = temp_o.astype(np.float32)
+    diff_o = img_float-templ_o_float
+    norm_o = np.mean(sum(abs(diff_o)))
+
+
+    if (norm_o < corr_o):
+      corr_o = norm_o
+
+
+  image_dir_x = r'C:\Users\Kumar\Desktop\Desktop\Python\icg-freshers-data-science-competition\Cross_Template'
+  image_files_x = glob.glob(image_dir_x + '/*.png')
+  corr_x = 1000000000000000
+
+  for x in image_files_x:
+    image = cv2.imread(x)
+    temp_x = transformImage(image)
+    templ_x_float = temp_x.astype(np.float32)
+    diff_x = img_float-templ_x_float
+    norm_x = np.mean(sum(abs(diff_x)))
+
+
+    if (norm_x < corr_x):
+      corr_x = norm_x
+
+
+  if (isEmpty(img)):
+      return 2
+  elif (corr_o < corr_x):
+      return 1
+  else:
+      return 0
+  
+image_dir = r'C:\Users\Kumar\Desktop\Desktop\Python\icg-freshers-data-science-competition\Dataset\Test'
+image_files = glob.glob(image_dir + '/*.png')
+
+for x in image_files:
+  image = cv2.imread(x)
+  index = int(Path(os.path.basename(x)).stem)
+  for x in range (0, 4495):
+    if (index == data.loc[x, 'ID']):
+      y = x
+  solution(image, y, data)
+  
+data.to_csv("pranav_solution_V3.csv", index=False)
+print(data.head(20))
---
+++ TicTacToe_Predictor.py
@@ -0,0 +1,110 @@
+import pandas as pd
+
+def pridict(grid, y):
+    end = False
+
+    def next_move_win(grid, player):
+        get_indexes = lambda x, gird: [i for (y, i) in zip(grid, range(len(grid))) if x == y]
+        for index in get_indexes(2, grid):
+            #print(index)
+            gridc2 = grid.copy()
+            gridc2[index] = player
+            #printgrid(gridc2)
+            for i in range(3):
+                if gridc2[3*i] == gridc2[1+ 3*i] and gridc2[1 + i*3] == gridc2[2 + i*3] and gridc2[i*3] == player:
+                    return True, index
+                elif gridc2[i+3] == gridc2[i+6] and gridc2[i] == gridc2[i+3] and gridc2[i] == player:
+                    return True, index
+            if (gridc2[0] == gridc2[4] and gridc2[8]== gridc2[4] and gridc2[4] == player) or (gridc2[2]== gridc2[4] and gridc2[6]== gridc2[4] and gridc2[4] == player):
+                    return True, index
+        return False, -1
+
+    def check_win(gridc):
+        for i in range(3):
+            if gridc[3*i] == gridc[1+ 3*i] and gridc[1 + i*3] == gridc[2 + i*3] and gridc[i*3] != 2:
+                return True, gridc[i*3]
+            elif gridc[i+3] == gridc[i+6] and gridc[i] == gridc[i+3] and gridc[i] != 2:
+                return True, gridc[i]
+        if (gridc[0] == gridc[4] and gridc[8]== gridc[4] and gridc[4] != 2) or (gridc[2]== gridc[4] and gridc[6]== gridc[4] and gridc[4] != 2):
+                return True, gridc[4]
+        return False, -1
+
+    def check_move(position, player):
+        gridc = grid.copy()
+        gridc[position] = player
+        #print("1 step")
+        #printgrid(gridc)
+        #print(check_win(gridc))
+        return check_win(gridc)
+        
+    def next_move(bpi, player):
+        #only one block left empty
+        if len(bpi) == 1:
+            grid[bpi[0]] = player
+            return check_win(grid)
+        if len(bpi) == 0:
+            return True, 2
+        for i in range(len(bpi)):
+            #print("yes")
+            result, who = check_move(bpi[i], player)
+            if (result and who == player):
+                return True, player
+        # if in next move opponent win
+        #print("yes")
+        next_move, to_move = next_move_win(grid, not player)
+        #print(next_move,to_move)
+        if(next_move):
+            grid[to_move] = player
+            #print("yes")
+            return False, -1
+        else:
+            #grid[bpi[0]] = player
+            return True, 2
+
+
+
+    def count(grid):
+        get_indexes = lambda x, gird: [i for (y, i) in zip(grid, range(len(grid))) if x == y]
+        #print(get_indexes(2,grid))
+        # X O blank
+        return grid.count(0), grid.count(1), grid.count(2), get_indexes(2, grid)
+
+    #main
+    while(not end):
+        #printgrid(grid)
+        X_count, O_count, blank_count, blank_position_index = count(grid);
+        if (X_count <= O_count):
+            # X play
+            #print("X play")
+            end, who = next_move(blank_position_index, 0)
+        else:
+            # O play
+            #print("O play")
+            end, who = next_move(blank_position_index, 1)
+
+    if(who == 0):
+        data.loc[y, 'Decision'] = 1
+        print("1, player")
+    elif(who == 1):
+        data.loc[y, 'Decision'] = 0
+        print("0, computer")
+    elif(who == 2):
+        data.loc[y, 'Decision'] = 2
+        print("tie")
+
+data = pd.read_csv(r"C:\Users\Kumar\Desktop\Desktop\Python\icg-freshers-data-science-competition\pranav_solution_V2.csv")
+for y in range (0, 4495):
+    symbol1 = data.loc[y, 'POS_1']
+    symbol2 = data.loc[y, 'POS_2']
+    symbol3 = data.loc[y, 'POS_3']
+    symbol4 = data.loc[y, 'POS_4']
+    symbol5 = data.loc[y, 'POS_5']
+    symbol6 = data.loc[y, 'POS_6']
+    symbol7 = data.loc[y, 'POS_7']
+    symbol8 = data.loc[y, 'POS_8']
+    symbol9 = data.loc[y, 'POS_9']
+    grid = [symbol1, symbol2, symbol3, symbol4, symbol5, symbol6, symbol7, symbol8, symbol9]
+    pridict(grid, y)
+    print(y)
+
+data.to_csv("pranav_solution_final_V2.csv", index=False)
